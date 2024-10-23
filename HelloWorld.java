46ab8a560c2cfb9a30e33c8539e80ba9bb2e36b1
HoXuanPhuong2417011 <phuonghoxuan197@gmail.com>
2024-10-23 22:55:03 +0700

    bai 1

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+//Text-printing program
+public class HelloWorld {
+
+    public static void main(String arg[]){ 
+        System.out.println("Xin chao \n cac ban");
+        System.out.println("Hello \t world!");
+
+    }
+}
